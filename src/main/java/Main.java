3455319cf5ec80fc4fc1f847d71e9ca121bef740
Ari <aki.vatanabe@gmail.com>
2025-04-18 15:13:41 +0600

    test: run database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import migration.LiquibaseMigrator;
+import util.DatabaseConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            LiquibaseMigrator.migrate();
+
+        } finally {
+            DatabaseConnection.closeConnection();
+        }
     }
 }
