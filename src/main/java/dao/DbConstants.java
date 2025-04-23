af0662a8ab5a929e173604972fb0a703753edf0b
Ari <aki.vatanabe@gmail.com>
2025-04-23 20:11:25 +0600

    feat: added table name constants

---
+++ src/main/java/dao/DbConstants.java
@@ -0,0 +1,19 @@
+package dao;
+
+public class DbConstants {
+    private DbConstants() {}
+    
+    public static final String ROLE_TABLE = "ROLES";
+    public static final String USER_TABLE = "USERS";
+    public static final String PRODUCT_CATEGORY_TABLE = "PRODUCT_CATEGORIES";
+    public static final String EXPENSE_CATEGORY_TABLE = "EXPENSE_CATEGORIES";
+    public static final String INCOME_SOURCE_TABLE = "INCOME_SOURCES";
+    public static final String PRODUCT_TABLE = "PRODUCTS";
+    public static final String STOCK_TABLE = "STOCK";
+    public static final String PURCHASE_TABLE = "PURCHASES";
+    public static final String SALE_TABLE = "SALES";
+    public static final String EXPENSE_TABLE = "EXPENSES";
+    public static final String INCOME_TABLE = "INCOMES";
+    public static final String PAYROLL_TABLE = "PAYROLLS";
+    public static final String MONTHLY_BUDGET_TABLE = "MONTHLY_BUDGETS";
+}
