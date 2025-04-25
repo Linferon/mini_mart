44892e9e3474c29d3797903eb25c1aff19df9dad
Ari <aki.vatanabe@gmail.com>
2025-04-25 17:42:03 +0600

    refactor: changed user table migration

--- src/main/resources/db/changelog/initial/002_create_users.sql
+++ src/main/resources/db/changelog/initial/002_create_users.sql
@@ -6,13 +6,14 @@ create table users
     surname    varchar(50),
     email      varchar(255) unique             not null,
     password   varchar(255)                    not null,
+    enabled    boolean                         not null default true,
     role_id    long                            not null,
     created_at timestamp                       not null default current_timestamp,
     updated_at timestamp                       not null default current_timestamp,
 
     constraint fk_user_role
         foreign key (role_id)
-            references roles(id)
+            references roles (id)
             on delete restrict
             on update cascade
 )
