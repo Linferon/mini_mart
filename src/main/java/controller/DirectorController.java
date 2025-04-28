8fee1f19161e927becc95d133f0dea74dc0b6a2c
Ari <aki.vatanabe@gmail.com>
2025-04-28 12:52:36 +0600

    optimized director controller

--- src/main/java/controller/DirectorController.java
+++ src/main/java/controller/DirectorController.java
@@ -5,11 +5,8 @@ import model.Payroll;
 import model.Role;
 import model.User;
 import service.*;
-import util.ConsoleUtil;
+import util.*;
 import exception.handler.ExceptionHandler;
-import util.InputHandler;
-import util.LoggerUtil;
-import util.TableFormatter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -20,172 +17,225 @@ public class DirectorController extends BaseController {
     private final UserService userService;
     private final MonthlyBudgetService budgetService;
     private final PayrollService payrollService;
+
     private final List<Role> roles;
+
+    private final BudgetController budgetController;
+    private final EmployeeController employeeController;
+    private final StatisticController statisticController;
 
     public DirectorController() {
         userService = UserService.getInstance();
         budgetService = MonthlyBudgetService.getInstance();
         payrollService = PayrollService.getInstance();
+
         roles = RoleService.getInstance().getAllRoles();
+
+        budgetController = new BudgetController();
+        employeeController = new EmployeeController();
+        statisticController = new StatisticController();
     }
 
     @Override
     public void showMenu() {
         createMenu("Меню Директора")
-                .addMenuItem("Назначить бюджет на месяц", this::setBudget)
-                .addMenuItem("Управление сотрудниками", this::manageEmployees)
-                .addMenuItem("Просмотр статистики", this::showStatistics)
+                .addMenuItem("Управление бюджетами", budgetController::manageBudgets)
+                .addMenuItem("Управление сотрудниками", employeeController::manageEmployees)
+                .addMenuItem("Просмотр статистики", statisticController::viewStatistics)
                 .addExitItem("Выйти из системы")
                 .show();
     }
 
-    private void manageEmployees() {
-        createMenu("Управление сотрудниками")
-                .addMenuItem("Показать всех сотрудников", this::showAllEmployees)
-                .addMenuItem("Зарегистрировать нового сотрудника", this::registerNewEmployee)
-                .addMenuItem("Изменить данные сотрудника", this::updateEmployee)
-                .addMenuItem("Уволить сотрудника или восстановить сотрудника", this::toggleEmployeeStatus)
-                .addExitItem("Назад")
-                .show();
-    }
+    private class BudgetController{
+        private void manageBudgets() {
+            createMenu("Управление бюджетами")
+                    .addMenuItem("Посмотреть все месячные бюджеты", this::viewAllBudgets)
+                    .addMenuItem("Посмотреть бюджет по временному диапазону", this::viewBudgetsByDateRange)
+                    .addMenuItem("Назначить бюджет на месяц", this::setBudget)
+                    .addMenuItem("Изменить бюджет", this::editBudget)
+                    .addExitItem("Назад")
+                    .show();
+        }
 
-    private void toggleEmployeeStatus() {
-        ExceptionHandler.execute(() -> {
-            List<User> users = userService.getAllUsers();
-            ConsoleUtil.showEntitiesTable(users, "Все сотрудники");
+        private void viewAllBudgets() {
+            ExceptionHandler.execute(() -> {
+                List<MonthlyBudget> budgets = budgetService.getAllBudgets();
+                ConsoleUtil.showEntitiesTable(budgets, "Список всех бюджетов на месяц: ");
+            });
+        }
 
-            Long userId = InputHandler.getLongInput("Введите ID сотрудника: ");
-            User user = userService.getUserById(userId);
-            validateSelfDismiss(user);
+        protected void viewBudgetsByDateRange(LocalDate startDate, LocalDate endDate) {
+            ExceptionHandler.execute(() -> {
+                List<MonthlyBudget> budgets = budgetService.getBudgetsByDateRange(startDate, endDate);
+                ConsoleUtil.printHeader("Статистика бюджета за период " + startDate + " - " + endDate);
+                ConsoleUtil.println(TableFormatter.formatTable(budgets));
+            });
+        }
 
-            user.setEnabled(!user.getEnabled());
-            userService.updateUser(user);
+        private void viewBudgetsByDateRange() {
+            ExceptionHandler.execute(() -> showDateRangeMenu(this::viewBudgetsByDateRange));
+        }
 
-            String action = Boolean.TRUE.equals(user.getEnabled()) ? "восстановлен" : "уволен";
-            showSuccess("Сотрудник успешно " + action + ".");
-        });
-    }
+        private void setBudget() {
+            ExceptionHandler.execute(() -> {
+                BigDecimal plannedIncome = InputHandler.getBigDecimalInput("Введите сумму планируемого дохода: ");
+                BigDecimal plannedExpenses = InputHandler.getBigDecimalInput("Введите сумму планируемых расходов: ");
+                LocalDate budgetDate = InputHandler.getValidBudgetDate();
 
-    private void showAllEmployees() {
-        ExceptionHandler.execute(() -> {
-            List<User> users = userService.getActiveUsers();
-            Map<String, Long> roleStats = userService.getRoleStats();
+                budgetService.createBudget(budgetDate, plannedIncome, plannedExpenses);
+                showSuccess("Бюджет успешно установлен.");
+            });
+        }
 
-            ConsoleUtil.showEntitiesTable(users, "Активные сотрудники");
+        private void editBudget() {
+            viewBudgetsByDateRange();
+            Long budgetId = InputHandler.getLongInput("Введите id бюджета для редактирования");
+            MonthlyBudget budget = budgetService.getBudgetById(budgetId);
+            ConsoleUtil.showEntityDetails(budget, "Редактирование бюджета");
 
-            ConsoleUtil.printHeader("Распределение сотрудников по ролям");
-            roleStats.forEach((role, count) ->
-                    ConsoleUtil.println(role + ": " + count + " чел.")
-            );
-        });
-    }
+            BigDecimal plannedIncome = InputHandler.getUpdatedBigDecimalValue("сумму планируемых доходов", budget.getPlannedIncome());
+            BigDecimal plannedExpense = InputHandler.getUpdatedBigDecimalValue("сумму планируемых расходов", budget.getPlannedExpenses());
+            LocalDate oldIncomeDate = budget.getBudgetDate();
+            LocalDate budgetDate = InputHandler.getUpdatedDateValue("дату бюджета", oldIncomeDate);
 
-    private void setBudget() {
-        ExceptionHandler.execute(() -> {
-            List<MonthlyBudget> currentBudgets = budgetService.getAllBudgets();
-            ConsoleUtil.showEntitiesTable(currentBudgets, "Текущие бюджеты");
-
-            BigDecimal plannedIncome = InputHandler.getBigDecimalInput("Введите сумму планируемого дохода: ");
-            BigDecimal plannedExpenses = InputHandler.getBigDecimalInput("Введите сумму планируемых расходов: ");
-            LocalDate budgetDate = InputHandler.getValidBudgetDate();
-
-            budgetService.createBudget(budgetDate, plannedIncome, plannedExpenses);
-            showSuccess("Бюджет успешно установлен.");
-        });
-    }
-
-
-    private void registerNewEmployee() {
-        ExceptionHandler.execute(() -> {
-            String name = InputHandler.getStringInput("Введите имя сотрудника: ");
-            String surname = InputHandler.getStringInput("Введите фамилию сотрудника: ");
-            String email = InputHandler.getStringInput("Введите email: ");
-            String password = InputHandler.getStringInput("Введите пароль: ");
-            Role selectedRole = selectRole();
-
-            User newUser = new User(null, name, surname, email, password, true, selectedRole, null, null);
-            userService.registerUser(newUser);
-            showSuccess("Сотрудник успешно зарегистрирован.");
-        });
-    }
-
-    private void updateEmployee() {
-        ExceptionHandler.execute(() -> {
-            showAllEmployees();
-            Long userId = InputHandler.getLongInput("Введите ID сотрудника для обновления: ");
-            User existingUser = userService.getUserById(userId);
-
-            ConsoleUtil.showEntityDetails(existingUser, "Текущие данные сотрудника");
-
-            InputHandler.inputIfPresent("Введите новое имя", existingUser::setName);
-            InputHandler.inputIfPresent("Введите новую фамилию", existingUser::setSurname);
-            InputHandler.inputIfPresent("Введите новый email", existingUser::setEmail);
-
-            if (!userService.getCurrentUser().equals(existingUser)) {
-                showConfirmationMenu("Хотите изменить роль? (да/нет): ", () -> {
-                    Role selectedRole = selectRole();
-                    existingUser.setRole(selectedRole);
-                });
+            if (budgetService.updateBudget(budgetId, plannedIncome, plannedExpense, budgetDate)) {
+                showSuccess("Доход успешно обновлен.");
+            } else {
+                showError("Не удалось обновить доход.");
             }
-
-            userService.updateUser(existingUser);
-            showSuccess("Данные сотрудника обновлены.");
-        });
+        }
     }
 
-    private void showStatistics() {
-        ExceptionHandler.execute(() -> showDateRangeMenu((startDate, endDate) -> {
-            List<MonthlyBudget> budgets = budgetService.getBudgetsByDateRange(startDate, endDate);
+    private class EmployeeController{
+        private void manageEmployees() {
+            createMenu("Управление сотрудниками")
+                    .addMenuItem("Показать всех сотрудников", this::viewAllEmployees)
+                    .addMenuItem("Зарегистрировать нового сотрудника", this::registerNewEmployee)
+                    .addMenuItem("Изменить данные сотрудника", this::updateEmployee)
+                    .addMenuItem("Уволить сотрудника или восстановить сотрудника", this::toggleEmployeeStatus)
+                    .addExitItem("Назад")
+                    .show();
+        }
 
-            ConsoleUtil.printHeader("Статистика бюджета за период " + startDate + " - " + endDate);
-            ConsoleUtil.println(TableFormatter.formatTable(budgets));
+        private void viewAllEmployees() {
+            ExceptionHandler.execute(() -> {
+                List<User> users = userService.getActiveUsers();
+                Map<String, Long> roleStats = userService.getRoleStats();
 
-            ConsoleUtil.printHeader("Сводка");
-            printBudgetSummary(startDate, endDate);
-            printPayrollsSummary(startDate, endDate);
-        }));
+                ConsoleUtil.showEntitiesTable(users, "Активные сотрудники");
+
+                ConsoleUtil.printHeader("Распределение сотрудников по ролям");
+                roleStats.forEach((role, count) ->
+                        ConsoleUtil.println(role + ": " + count + " чел.")
+                );
+            });
+        }
+
+        private void registerNewEmployee() {
+            ExceptionHandler.execute(() -> {
+                String name = InputHandler.getStringInput("Введите имя сотрудника: ");
+                String surname = InputHandler.getStringInput("Введите фамилию сотрудника: ");
+                String email = InputHandler.getStringInput("Введите email: ");
+                String password = InputHandler.getStringInput("Введите пароль: ");
+                Role selectedRole = selectRole();
+
+                User newUser = new User(null, name, surname, email, password, true, selectedRole, null, null);
+                userService.registerUser(newUser);
+                showSuccess("Сотрудник успешно зарегистрирован.");
+            });
+        }
+
+        private void updateEmployee() {
+            ExceptionHandler.execute(() -> {
+                viewAllEmployees();
+                Long userId = InputHandler.getLongInput("Введите ID сотрудника для обновления: ");
+                User existingUser = userService.getUserById(userId);
+
+                ConsoleUtil.showEntityDetails(existingUser, "Текущие данные сотрудника");
+
+                InputHandler.inputIfPresent("Введите новое имя", existingUser::setName);
+                InputHandler.inputIfPresent("Введите новую фамилию", existingUser::setSurname);
+                InputHandler.inputIfPresent("Введите новый email", existingUser::setEmail);
+
+                if (!userService.getCurrentUser().equals(existingUser)) {
+                    showConfirmationMenu("Хотите изменить роль? ", () -> {
+                        Role selectedRole = selectRole();
+                        existingUser.setRole(selectedRole);
+                    });
+                }
+
+                userService.updateUser(existingUser);
+                showSuccess("Данные сотрудника обновлены.");
+            });
+        }
+
+        private void toggleEmployeeStatus() {
+            ExceptionHandler.execute(() -> {
+                viewAllEmployees();
+                Long userId = InputHandler.getLongInput("Введите ID сотрудника: ");
+                User user = userService.getUserById(userId);
+
+                validateSelfDismiss(user);
+                user.setEnabled(!user.getEnabled());
+                userService.updateUser(user);
+
+                String action = Boolean.TRUE.equals(user.getEnabled()) ? "восстановлен" : "уволен";
+                showSuccess("Сотрудник успешно " + action + ".");
+            });
+        }
+
+        private Role selectRole() {
+            ConsoleUtil.showEntitiesTable(roles, "Доступные роли");
+            int roleChoice = InputHandler.getIntInput("Введите номер роли: ");
+            return roles.get(roleChoice - 1);
+        }
+
+        private void validateSelfDismiss(User user) {
+            if (userService.getCurrentUser().equals(user)) {
+                LoggerUtil.error("Вы не можете уволить самого себя!");
+            }
+        }
     }
 
-    private void printPayrollsSummary(LocalDate start, LocalDate end) {
-        List<Payroll> payrolls = payrollService.getPayrollsByPeriod(start, end);
+    private class StatisticController{
+        private void viewStatistics() {
+            ExceptionHandler.execute(() -> showDateRangeMenu((startDate, endDate) -> {
+                budgetController.viewBudgetsByDateRange(startDate, endDate);
+                ConsoleUtil.printHeader("Сводка");
+                printBudgetSummary(startDate, endDate);
+                printPayrollsSummary(startDate, endDate);
+            }));
+        }
 
-        long totalPayrolls = payrolls.size();
-        long paidPayrolls = payrolls.stream().filter(Payroll::isPaid).count();
-        long unpaidPayrolls = totalPayrolls - paidPayrolls;
+        private void printPayrollsSummary(LocalDate start, LocalDate end) {
+            List<Payroll> payrolls = payrollService.getPayrollsByPeriod(start, end);
 
-        ConsoleUtil.printHeader("Статистика зарплат за период " + start + " - " + end);
-        ConsoleUtil.println("Всего начислений: " + totalPayrolls);
-        ConsoleUtil.println("Выплачено: " + paidPayrolls + " (" + (paidPayrolls * 100 / totalPayrolls) + "%)");
-        ConsoleUtil.println("Не выплачено: " + unpaidPayrolls + " (" + (unpaidPayrolls * 100 / totalPayrolls) + "%)");
-    }
+            long totalPayrolls = payrolls.size();
+            long paidPayrolls = payrolls.stream().filter(Payroll::isPaid).count();
+            long unpaidPayrolls = totalPayrolls - paidPayrolls;
+
+            ConsoleUtil.printHeader("Статистика зарплат за период " + start + " - " + end);
+            ConsoleUtil.println("Всего начислений: " + totalPayrolls);
+            ConsoleUtil.println("Выплачено: " + paidPayrolls + " (" + (paidPayrolls * 100 / totalPayrolls) + "%)");
+            ConsoleUtil.println("Не выплачено: " + unpaidPayrolls + " (" + (unpaidPayrolls * 100 / totalPayrolls) + "%)");
+        }
 
 
-    private void printBudgetSummary(LocalDate start, LocalDate end) {
-        BigDecimal plannedIncome = budgetService.getTotalPlannedIncome(start, end);
-        BigDecimal actualIncome = budgetService.getTotalActualIncome(start, end);
-        BigDecimal plannedExpenses = budgetService.getTotalPlannedExpenses(start, end);
-        BigDecimal actualExpenses = budgetService.getTotalActualExpenses(start, end);
+        private void printBudgetSummary(LocalDate start, LocalDate end) {
+            BigDecimal plannedIncome = budgetService.getTotalPlannedIncome(start, end);
+            BigDecimal actualIncome = budgetService.getTotalActualIncome(start, end);
+            BigDecimal plannedExpenses = budgetService.getTotalPlannedExpenses(start, end);
+            BigDecimal actualExpenses = budgetService.getTotalActualExpenses(start, end);
 
-        BigDecimal plannedProfit = plannedIncome.subtract(plannedExpenses);
-        BigDecimal actualProfit = actualIncome.subtract(actualExpenses);
+            BigDecimal plannedProfit = plannedIncome.subtract(plannedExpenses);
+            BigDecimal actualProfit = actualIncome.subtract(actualExpenses);
 
-        ConsoleUtil.println("Плановый доход: " + plannedIncome);
-        ConsoleUtil.println("Плановые расходы: " + plannedExpenses);
-        ConsoleUtil.println("Фактический доход: " + actualIncome);
-        ConsoleUtil.println("Фактические расходы: " + actualExpenses);
-        ConsoleUtil.println("Плановая прибыль: " + plannedProfit);
-        ConsoleUtil.println("Фактическая прибыль: " + actualProfit);
-    }
-
-    private Role selectRole() {
-        ConsoleUtil.showEntitiesTable(roles, "Доступные роли");
-        int roleChoice = InputHandler.getIntInput("Введите номер роли: ");
-        return roles.get(roleChoice - 1);
-    }
-
-    private void validateSelfDismiss(User user) {
-        if (userService.getCurrentUser().equals(user)) {
-            LoggerUtil.error("Вы не можете уволить самого себя!");
+            ConsoleUtil.println("Плановый доход: " + plannedIncome);
+            ConsoleUtil.println("Плановые расходы: " + plannedExpenses);
+            ConsoleUtil.println("Фактический доход: " + actualIncome);
+            ConsoleUtil.println("Фактические расходы: " + actualExpenses);
+            ConsoleUtil.println("Плановая прибыль: " + plannedProfit);
+            ConsoleUtil.println("Фактическая прибыль: " + actualProfit);
         }
     }
 }
