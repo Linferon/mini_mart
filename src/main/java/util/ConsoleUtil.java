b22f88bf13e579cf393a6267f16862a031b5fa1c
Ari <aki.vatanabe@gmail.com>
2025-04-29 18:01:41 +0600

    refactor: improved ConsoleUtil

--- src/main/java/util/ConsoleUtil.java
+++ src/main/java/util/ConsoleUtil.java
@@ -1,7 +1,15 @@
 package util;
 
+import model.FormattableEntity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static util.InputHandler.getDateInput;
+
 public class ConsoleUtil {
-    private ConsoleUtil() {}
+    private ConsoleUtil() {
+    }
 
     public static void println(Object obj) {
         System.out.println(obj);
@@ -23,5 +31,28 @@ public class ConsoleUtil {
 
     public static void printError(String s) {
         System.out.println(s);
+    }
+
+    public static void showEntityDetails(Object entity, String title) {
+        printHeader(title);
+        println(entity.toString());
+    }
+
+    @FunctionalInterface
+    public interface DateRangeSupplier<T> {
+        List<T> getForDateRange(LocalDate startDate, LocalDate endDate);
+    }
+
+    public static <T extends FormattableEntity> void selectEntitiesForDateRange(DateRangeSupplier<T> supplier, String headerMessage) {
+        LocalDate startDate = getDateInput("Введите период для поиска" + " (начальная дата, ГГГГ-ММ-ДД): ");
+        LocalDate endDate = getDateInput("Введите период для поиска" + " (конечная дата, ГГГГ-ММ-ДД): ");
+
+        List<T> entities = supplier.getForDateRange(startDate, endDate);
+        showEntitiesTable(entities, headerMessage);
+    }
+
+    public static <T extends FormattableEntity> void showEntitiesTable(List<T> entities, String title) {
+        printHeader(title);
+        println(TableFormatter.formatTable(entities));
     }
 }
